5dab5bbec1ea92ff03f8557fb0b2db3de4fd292e
anas <anasalialmora@gmail.com>
2021-10-04 02:45:07 +0530

    adding solution to problem 3

---
+++ answer-3/soln3.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+
+// comparator for our sort function
+bool compare(const vector<string> &first, const vector<string> &other) {
+	if (stoi(first[3]) == stoi(other[3])) {
+		return stoi(first[2]) > stoi(other[2]);
+	}
+	return stoi(first[3]) > stoi(other[3]);
+}
+
+int main() {
+	ifstream inputFile;
+	inputFile.open("../input/question-3/main.csv");
+	ofstream outputFile;
+	outputFile.open("./main.csv");
+
+	unordered_set<int> isUsefulCol = {0, 30, 31};
+
+	// read and ignore titles row
+	if (inputFile.good()) {
+		string line;
+		getline(inputFile, line, '\n');
+	}
+
+	vector<vector<string>> allData;
+
+	int index = 0;
+	while (inputFile.good()) {
+		vector<string> temp(35);
+		vector<string> usefulContent;
+		usefulContent.push_back(to_string(index));
+		for (int i = 0; i < 35; i++) {
+			if ( i == 34 ) {
+				getline(inputFile, temp[i], '\n');
+			} else {
+				getline(inputFile, temp[i], ',');
+			}
+			if (isUsefulCol.find(i) != isUsefulCol.end()) {
+				usefulContent.push_back(temp[i]);
+			}
+		}
+		allData.push_back(usefulContent);
+
+		index++;
+	}
+
+	sort(allData.begin(), allData.end(), compare);
+
+
+	// start writing the output file
+	outputFile << "" << "," << "Team" << "," << "Yellow Cards" << "," << "Red Cards" << '\n';
+	for (size_t i = 0; i < allData.size(); i++) {
+		for (auto col : allData[i]) {
+			outputFile << col << ",";
+		}
+		outputFile << '\n';
+	}
+
+	return 0;
+}
